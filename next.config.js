85e8be0e54c8b2e2180627c1ce4fbb59e8a6fdce
shuvamk <kshuvam564@gmail.com>
2021-06-30 23:34:25 +0530

    Css config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["media-exp3.licdn.com"],
   },
+  assetPrefix: isProd ? "/react-hire" : "",
 };
