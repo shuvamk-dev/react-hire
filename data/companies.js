d26ef9495c312a4b95097db83fe3137f3b87a487
shuvamk <kshuvam564@gmail.com>
2021-06-30 19:56:16 +0530

    Added first 5 companies

---
+++ data/companies.js
@@ -0,0 +1,37 @@
+export const Companies = [
+  {
+    id: 1,
+    name: "WazirX",
+    webiste: "https://careers.wazirx.com/jobs",
+    domain: "Fintech",
+    logo: "https://media-exp3.licdn.com/dms/image/C4E0BAQHfjTMcCunuOw/company-logo_200_200/0/1623824571975?e=1632960000&v=beta&t=_pCbSyYClLIXcJlTI7ohW5Z1bqVMCqPlmyo5vnTdRwU",
+  },
+  {
+    id: 2,
+    name: "Groww",
+    website: "https://groww.in/about-us",
+    domain: "Fintech",
+    logo: "https://media-exp3.licdn.com/dms/image/C4D0BAQHSl3byAWi9ow/company-logo_200_200/0/1519952564040?e=1632960000&v=beta&t=wtQEncFEZ1pMtFFPNaHb9hGd7UyZ5trVXBAkd2p0v1I",
+  },
+  {
+    id: 3,
+    name: "HackerEarth",
+    webiste: "https://hackerearthjobs.recruiterbox.com/",
+    domain: "Software Company",
+    logo: "https://media-exp3.licdn.com/dms/image/C4D0BAQHgoMowHLH6Rw/company-logo_200_200/0/1622540336050?e=1632960000&v=beta&t=uPB6QAiQxl9kgeuLpTNFfJXRsuacPlGfgwTk82Pd2KA",
+  },
+  {
+    id: 4,
+    name: "Zebpay",
+    webiste: "https://zebpay.com/careers/",
+    domain: "Fintech",
+    logo: "https://media-exp3.licdn.com/dms/image/C560BAQFsTTpZZeQAtg/company-logo_200_200/0/1583130938756?e=1632960000&v=beta&t=ZSolQDLUlpodIlrxADKJZdeujcbJyqSXXGsOUP1eZ04",
+  },
+  {
+    id: 5,
+    name: "Recko Inc.",
+    website: "https://recko-team.freshteam.com/jobs",
+    domain: "Fintech",
+    logo: "https://media-exp3.licdn.com/dms/image/C560BAQFkP-cSbCe28A/company-logo_200_200/0/1604400287450?e=1632960000&v=beta&t=E-W8I9pSx7wViKD9--ICAg00gsPKbe6dCTxiW2AxMDI",
+  },
+];
