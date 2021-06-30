ab39e9091f5278422b83ce7e841cad2520b45df5
shuvamk <kshuvam564@gmail.com>
2021-07-01 01:10:58 +0530

    Added bookmark icon

--- components/home/company-card/index.js
+++ components/home/company-card/index.js
@@ -21,11 +21,16 @@ function CompanyCard({ company }) {
         </div>
       </div>
       <div className={styles.cardRight}>
-        <a href={company.website} target="_blank" rel="noopener noreferrer">
-          <div className={styles.visit}>
-            <i className="fi-rr-globe"></i>Visit
+        <div className={styles.options}>
+          <div className={styles.bookmarkIcon}>
+            <i className="fi-rr-bookmark"></i>
           </div>
-        </a>
+          <a href={company.website} target="_blank" rel="noopener noreferrer">
+            <div className={styles.visit}>
+              <i className="fi-rr-globe"></i>Visit
+            </div>
+          </a>
+        </div>
         <div className={styles.domain}>{company.domain}</div>
       </div>
     </div>
