b627d3403d7bf4652763ae9b9ba58713a07430f8
shuvamk <42249560+shuvamk@users.noreply.github.com>
2021-07-01 17:12:43 +0000

    Deploying to gh-pages from  @ b6a4400c5e4b0e61d821f15865fba435873d374f 🚀

--- _next/static/PXtdj07Qomcvtk9Br6ZI9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a3652bab81b2dc331dcb.css","static/chunks/pages/index-daffe8b67b00d823f152.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
